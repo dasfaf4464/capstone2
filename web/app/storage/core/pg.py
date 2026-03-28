a09bcfbc3b193ca264a62cb425ea2737e8dae95c
dasfaf4464 <richchc@gmail.com>
2026-03-29 00:07:54 +0900

    db - postgresql : 연결 테스트용 코드 작성

--- web/app/storage/core/pg.py
+++ web/app/storage/core/pg.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import os
 
@@ -18,3 +19,28 @@ def init_pg(app):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     pg_alchemy.init_app(app)
+
+def test_raw_db_connection():
+    db = os.getenv("POSTGRES_DB", "capstone_db")
+    user = os.getenv("FLASK_DB_ID", "flask_server")
+    pw = os.getenv("FLASK_DB_PW")
+    host = os.getenv("DB_HOST", "localhost") 
+
+    if not pw:
+        raise ValueError("❌ DB 비밀번호(FLASK_DB_PW)가 설정되지 않았습니다.")
+
+    db_url = f'postgresql://{user}:{pw}@{host}:5432/{db}'
+    
+    engine = create_engine(db_url)
+
+    try:
+        with engine.connect() as connection:
+            query = text("SELECT * FROM test;")
+            result = connection.execute(query).fetchone()
+            print(f"연결 성공: {result[0]}")
+
+    except Exception as e:
+        print(f"연결 실패: {e}")
+
+if __name__ == "__main__":
+    test_raw_db_connection()
