3fdabe07ac17f7c07106d263126b4a65daaa2f93
LHG202014981 <lhg0347@naver.com>
2026-04-03 21:33:00 +0900

    db - 테이블 수정 : users, images, temp_video 테이블 수정 및 나머지 테이블 주석 처리

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -14,28 +14,33 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO flask_server;
 
 -- 회원 정보 테이블
 CREATE TABLE IF NOT EXISTS users (
-    user_nickname VARCHAR(30) NOT NULL UNIQUE,
+    user_id VARCHAR(30) NOT NULL UNIQUE, -- nickname에서 id로
     user_pw VARCHAR(30) NOT NULL,
-    user_email VARCHAR(50) NOT NULL,
-    user_uuid uuid PRIMARY KEY DEFAULT gen_random_uuid()
-    user_create_time timestampz DEFAULT now()
+    -- user_email VARCHAR(50) NOT NULL,
+    user_uuid uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_create_time timestamptz DEFAULT now()
 );
 
+/*
 -- 태그 테이블
 CREATE TABLE IF NOT EXISTS image_tags (
     tag_uuid uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     tag_name VARCHAR(50) UNIQUE NOT NULL
 );
+*/
 
 -- 사진 저장 테이블
 CREATE TABLE IF NOT EXISTS images (
     image_uuid uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    image_path TEXT NOT NULL,
-    image_query TEXT,
-    image_create_time timestampz DEFAULT now()
-    user_uuid INTEGER REFERENCES users(user_uuid) ON DELETE CASCADE
+    image_path TEXT NOT NULL, --서버 저장 경로
+    image_query TEXT NOT NULL, -- 사용자 입력 문장
+    image_description TEXT, -- llm 설명 저장
+    image_create_time timestamptz DEFAULT now(),
+    is_selected BOOLEAN NOT NULL DEFAULT FALSE, -- 5장의 사진을 위한 임시 사진 테이블이 필요한지 아니면 다 사진에 저장하고 한장만 선택해서 true로 바꿀지...
+    user_uuid UUID REFERENCES users(user_uuid) ON DELETE CASCADE -- 외래키 쓸 떄 타입 같아야 한다고 합니다
 );
 
+/*
 -- 사진-태그 매핑 테이블
 CREATE TABLE IF NOT EXISTS image_tag_map (
     image_uuid uuid REFERENCES images(image_uuid) ON DELETE CASCADE,
@@ -43,15 +48,19 @@ CREATE TABLE IF NOT EXISTS image_tag_map (
     
     PRIMARY KEY (image_uuid, tag_uuid)
 );
+*/
 
 CREATE TABLE IF NOT EXISTS temp_video (
-    video_uuid uuid PRIMARY KEY DEFAULT gen_random_uuid()
-    video_file_name TEXT UNIQUE NOT NULL
-    video_save_time timestampz DEFAULT now()
+    video_uuid uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    video_path TEXT NOT NULL, -- 서버 저장 경로
+    video_save_time timestamptz DEFAULT now(),
+    user_uuid UUID REFERENCES users(user_uuid) ON DELETE CASCADE
 );
 
+/*
 CREATE TABLE if NOT EXISTS temp_image (
     image_uuid uuid PRIMARY KEY DEFAULT gen_random_uuid()
     image_file_name TEXT UNIQUE NOT NULL
     image_save_time timestampz DEFAULT now()
 );
+ */
