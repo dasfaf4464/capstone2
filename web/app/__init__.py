cd53fa765e45926746dc3f1a02893fb687ad77c9
skcy1515 <skcy151515@gmail.com>
2026-04-06 15:09:03 +0900

    요구사항에 따라 api 코드 수정

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,44 +1,37 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 from flask import Flask
+from flask_cors import CORS
+# DB 매니저 불러오기
+from .storage.core.pg import init_pg 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
-    # 수정: 현재 파일(__init__.py)의 위치를 기준으로 절대 경로를 엶
-    # 파일을 저장할 수 있도록 폴더를 세팅
-    base_dir = os.path.abspath(os.path.dirname(__file__))
-    # todo: 이미지 저장 경로 따로 생성했으니 테스트 바람
+    init_pg(app)
 
-    media_base = os.getenv("MEDIA_STORAGE_BASE", "media")
-    uploads_dir = os.getenv("MEDIA_STORAGE_UPLOADS", "uploads")
-    results_dir = os.getenv("MEDIA_STORAGE_RESULTS", "results")
-    save_dir = os.getenv("MEDIA_STORAGE_USERS", "users") # -> 영구 저장용
-
-    app.config['UPLOAD_FOLDER'] = os.path.join(media_base, uploads_dir)
-    app.config['RESULT_FOLDER'] = os.path.join(media_base, results_dir)
-    app.config['SAVE_FOLDER'] = os.path.join(media_base, save_dir) # -> 영구 저장용 새로 만든 폴더
+    # .env 파일의 환경 변수를 읽어와서 동적으로 경로 설정
+    storage_base = os.environ.get('MEDIA_STORAGE_BASE', '/media')
+    upload_folder_name = os.environ.get('MEDIA_STORAGE_UPLOADS', 'uploads')
+    result_folder_name = os.environ.get('MEDIA_STORAGE_RESULTS', 'results')
+    
+    # 최종 물리 경로 조합 (예: /media/uploads)
+    app.config['UPLOAD_FOLDER'] = os.path.join(storage_base, upload_folder_name)
+    app.config['RESULT_FOLDER'] = os.path.join(storage_base, result_folder_name)
     app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024  # 500MB
 
-    # 폴더가 없으면 미리 생성
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     os.makedirs(app.config['RESULT_FOLDER'], exist_ok=True)
-    os.makedirs(app.config['SAVE_FOLDER'], exist_ok=True) # -> 영구 저장용 새로 만든 폴더
 
-    # 블루프린트들 불러오기
+    # 블루프린트 등록
     from .routes.main import main_bp
     from .routes.auth import auth_bp
-    from .routes.video import video_bp, analysis_bp
-    from .routes.archive import archive_bp
+    from .routes.video import video_bp
+    from .routes.image import image_bp
 
-    # 메인 앱에 블루프린트 등록
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(video_bp)
-    app.register_blueprint(analysis_bp)
-    app.register_blueprint(archive_bp)
+    app.register_blueprint(image_bp)
 
     return app
