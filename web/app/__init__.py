a73b2343289447310bb589b3071a1cde01563e67
dasfaf4464 <richchc@gmail.com>
2026-04-10 11:14:49 +0900

    fix - directory env fix

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -13,31 +13,34 @@ def create_app():
     init_pg(app)
 
     temp_dir = os.getenv("MEDIA_STORAGE_TEMP", "temp")
+    media_base = os.getenv("MEDIA_STORAGE_BASE", "media")
+    uploads_dir = os.getenv("MEDIA_STORAGE_UPLOADS", "uploads")
+    results_dir = os.getenv("MEDIA_STORAGE_RESULTS", "results")
+    save_dir = os.getenv("MEDIA_STORAGE_USERS", "users")
 
     app.config['UPLOAD_FOLDER'] = os.path.join(media_base, uploads_dir)
     app.config['RESULT_FOLDER'] = os.path.join(media_base, results_dir)
-    app.config['SAVE_FOLDER'] = os.path.join(media_base, save_dir) # -> 영구 저장용 새로 만든 폴더
+    app.config['SAVE_FOLDER'] = os.path.join(media_base, save_dir)
     app.config['IMAGES_FOLDER'] = app.config['SAVE_FOLDER']
     app.config['TEMP_FOLDER'] = os.path.join(media_base, temp_dir)
     app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024  # 500MB
 
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     os.makedirs(app.config['RESULT_FOLDER'], exist_ok=True)
-    os.makedirs(app.config['SAVE_FOLDER'], exist_ok=True) # -> 영구 저장용 새로 만든 폴더
+    os.makedirs(app.config['SAVE_FOLDER'], exist_ok=True)
     os.makedirs(app.config['TEMP_FOLDER'], exist_ok=True)
 
     # 블루프린트 등록
     from .routes.main import main_bp
     from .routes.auth import auth_bp
-    from .routes.video import video_bp, analysis_bp
-    from .routes.archive import archive_bp
+    from .routes.video import video_bp
+    from .routes.images import image_bp
     from .routes.media import media_bp
 
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(video_bp)
-    app.register_blueprint(analysis_bp)
-    app.register_blueprint(archive_bp)
+    app.register_blueprint(image_bp)
     app.register_blueprint(media_bp)
 
     return app
